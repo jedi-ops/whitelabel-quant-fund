51f0be2bd24757f80801224eb655afc3e6cfb1e6
tobalo <tobalo@yeetum.com>
2025-12-03 16:51:07 -0600

    init: quant whitelabel service

---
+++ README.md
@@ -0,0 +1,11 @@
+# White Label Quant Fund
+
+## Setup
+```sh
+uv sync
+```
+
+## Run
+```sh
+uv run -m fund
+```
---
+++ fund.py
@@ -0,0 +1,6 @@
+def main():
+    print("White label Quant Shit Loading...")
+
+
+if __name__ == "__main__":
+    main()
